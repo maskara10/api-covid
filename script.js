09dd9d4f8592a4e2ef914d2cc2d8945e2d53c554
maskara10 <vinivini147147147@gmail.com>
2023-05-04 23:32:45 -0300

    Add logina pagina pais

--- script.js
+++ script.js
@@ -8,56 +8,58 @@ axios
     let tConfimardos = document.getElementById("totalConf");
     tConfimardos.innerHTML = `
   <p>Total de Confirmados</p>
-        <span>${dadosGlobal.TotalConfirmed.toLocaleString('pt-BR')}</span>
+        <span>${dadosGlobal.TotalConfirmed.toLocaleString("pt-BR")}</span>
   `;
 
     let tMortes = document.getElementById("totalMort");
     tMortes.innerHTML = `
 <p>Total de Mortes</p>
-      <span>${dadosGlobal.TotalDeaths.toLocaleString('pt-BR')}</span>
+      <span>${dadosGlobal.TotalDeaths.toLocaleString("pt-BR")}</span>
 `;
 
     let tRecuperados = document.getElementById("totalRec");
     tRecuperados.innerHTML = `
 <p>Total Recuperados</p>
-    <span>${dadosGlobal.TotalRecovered.toLocaleString('pt-BR')}</span>
+    <span>${dadosGlobal.TotalRecovered.toLocaleString("pt-BR")}</span>
 `;
 
-    const ctx = document.getElementById('chartPizza');
+    const ctx = document.getElementById("chartPizza");
 
     new Chart(ctx, {
-      type: 'pie',
+      type: "pie",
       data: {
-        labels: [
-          'Confirmados',
-          'Recuperados',
-          'Mortes'
+        labels: ["Confirmados", "Recuperados", "Mortes"],
+        datasets: [
+          {
+            label: "My First Dataset",
+            data: [
+              dadosGlobal.NewConfirmed,
+              dadosGlobal.NewRecovered,
+              dadosGlobal.NewDeaths,
+            ],
+            backgroundColor: [
+              "rgb(255, 99, 132)",
+              "rgb(54, 162, 235)",
+              "rgb(255, 205, 86)",
+            ],
+            hoverOffset: 4,
+          },
         ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [dadosGlobal.NewConfirmed, dadosGlobal.NewRecovered, dadosGlobal.NewDeaths],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
       },
       options: {
         plugins: {
           title: {
             display: true,
-            text: 'Distribuiçaõ de Novos Casos'
-          }
-        }
-      }
+            text: "Distribuiçaõ de Novos Casos",
+          },
+        },
+      },
     });
 
-    const ctx1 = document.getElementById('chartBarra');
+    const ctx1 = document.getElementById("chartBarra");
 
     new Chart(ctx1, {
-      type: 'bar',
+      type: "bar",
       data: {
         labels: [
           dadosPais[0].Country,
@@ -69,52 +71,127 @@ axios
           dadosPais[6].Country,
           dadosPais[7].Country,
           dadosPais[8].Country,
-          dadosPais[9].Country
+          dadosPais[9].Country,
         ],
-        datasets: [{
-          label: 'Total de Mortes',
-          data: [
-            dadosPais[0].TotalDeaths,
-            dadosPais[1].TotalDeaths,
-            dadosPais[2].TotalDeaths,
-            dadosPais[3].TotalDeaths,
-            dadosPais[4].TotalDeaths,
-            dadosPais[5].TotalDeaths,
-            dadosPais[6].TotalDeaths,
-            dadosPais[7].TotalDeaths,
-            dadosPais[8].TotalDeaths,
-            dadosPais[9].TotalDeaths
-          ],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)'
-  
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)'
-    
-          ],
-          borderWidth: 1
-        }]
+        datasets: [
+          {
+            label: "Total de Mortes",
+            data: [
+              dadosPais[0].TotalDeaths,
+              dadosPais[1].TotalDeaths,
+              dadosPais[2].TotalDeaths,
+              dadosPais[3].TotalDeaths,
+              dadosPais[4].TotalDeaths,
+              dadosPais[5].TotalDeaths,
+              dadosPais[6].TotalDeaths,
+              dadosPais[7].TotalDeaths,
+              dadosPais[8].TotalDeaths,
+              dadosPais[9].TotalDeaths,
+            ],
+            backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+            borderColor: ["rgb(255, 99, 132)"],
+            borderWidth: 1,
+          },
+        ],
       },
       options: {
         plugins: {
           title: {
             display: true,
-            text: 'Total de Mortes por País - Top 10'
-          }
+            text: "Total de Mortes por País - Top 10",
+          },
         },
         scales: {
           y: {
-            beginAtZero: true
-          }
-        }
-      }
+            beginAtZero: true,
+          },
+        },
+      },
     });
-    console.log(dadosPais)
+    console.log(dadosPais);
+
+
   })
   .catch(function (error) {
     console.log(error);
   });
 
+  
 
-  //TotalDeaths
+  const dataInicio = document.getElementById("dataInicio");
+  const dataFim = document.getElementById("dataFim");
+  const pais = document.getElementById("paisOrigem");
+  const btnBuscar = document.getElementById("buscar");
+  const chartElement = document.getElementById("chartLine");
+
+
+  let chart;
+
+  function filterDataByDate(data, startDate, endDate) {
+    return data.filter((item) => {
+      const itemDate = new Date(item.Date);
+      return itemDate >= new Date(startDate) && itemDate <= new Date(endDate);
+    });
+  }
+
+  btnBuscar.addEventListener("click", () => {
+    const country = pais.value;
+    const startDate = dataInicio.value;
+    const endDate = dataFim.value;
+
+    if (!country) {
+      alert("Informe um país.");
+      return;
+    }
+
+    if (!startDate || !endDate) {
+      alert("Selecione um intervalo de datas.");
+      return;
+    }
+
+    axios
+      .get(
+        `https://api.covid19api.com/country/${country}?from=${startDate}T00:00:00Z&to=${endDate}T00:00:00Z`
+      )
+      .then((response) => {
+        const rawDate = response.data;
+        const filterData = filterDataByDate(rawDate, startDate, endDate);
+        const labels = [];
+        const values = [];
+        console.log(rawDate)
+
+        filterData.forEach((element) => {
+          labels.push(element.Date.slice(0, 10));
+          values.push(element.Confirmed);
+        });
+
+        if (chart) {
+          chart.destroy();
+        }
+
+        chart = new Chart(chartElement, {
+          type: "line",
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                label: "Casos Confirmados",
+                data: values,
+                backgroundColor: "rgba(75, 192, 192, 0.2)",
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 1
+              },
+            ],
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        })
+      }).catch((err) => { err });
+  });
+
+//TotalDeaths
